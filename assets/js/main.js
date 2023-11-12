5057b03697575163f8d7c9297882666db47a2ea1
Aziz Amari <azizamari2003@gmail.com>
2023-11-12 16:21:32 +0100

    mmake hamburger work

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+const links=document.querySelectorAll('.side_links')
+function openSlideMenu(){
+    document.getElementById('side-menu').style.width ='200px';
+    for(const link of links){
+        link.style.display ='block';
+    }
+}
+
+function closeSlideMenu(){
+    for(const link of links){
+        link.style.display ='none';
+    }
+    document.getElementById('side-menu').style.width ='0px';
+}
+
+for(const link of links){
+    link.addEventListener('click',()=>{
+        closeSlideMenu();
+    })
+}
+
+
+function saveToStorage(){
+    if(document.querySelector("textarea").value=="") return false;
+    localStorage.setItem('idea',document.querySelector("textarea").value);
+    return true;
+}
